6ecaac8aa91f4a123d4d9d44d094afa837d6cdde
andbudha <andreibartov@gmail.com>
2023-03-28 19:51:54 +0200

    BookShow comp. updated

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {BookEdit} from "./BookEdit";
 
 export const BookShow = ({book, onDelete}) => {
 
+    //title editor state
+    const [showEdit, setShowEdit]=useState(false);
+
     //book deleting by id func
-    const onClikHandler = () => {
+    const onClickDeleteHandler = () => {
         onDelete(book.id);
     }
+
+    //title editing func
+    const onClickEditHandler = () => {
+        setShowEdit(!showEdit);
+    }
+
+    //title&title-editing conditioning
+    let content = <h3>{book.title}</h3>
+
+    if(showEdit){
+        content = <BookEdit/>;
+    }
+
     return (
         <div className={'book-show'}>
-            {book.title}
+            {content}
             <div className={'actions'}>
-                <button className={'delete'} onClick={onClikHandler}>
-                    X
+                <button className={'edit'} onClick={onClickEditHandler}>
+                    Edit
+                </button>
+                <button className={'delete'} onClick={onClickDeleteHandler}>
+                    Delete
                 </button>
             </div>
         </div>
