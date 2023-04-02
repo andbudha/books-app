87bc72791abbda6cd2863b28bcee1649d6b64017
andbudha <andreibartov@gmail.com>
2023-04-02 14:29:08 +0200

    context data consumed(as an example)

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import {useContext} from "react";
+import {BooksContext} from "../context/books";
 import {BookShow} from "./BookShow";
 
 export const BookList = ({books, onDelete, editTitle}) => {
+
+    const number = useContext(BooksContext);
 
     const renderedBooks = books.map(book=>{
         return <BookShow key={book.id} book={book} onDelete={onDelete} editTitle={editTitle}/>
     })
     return (
         <div className={'book-list'}>
+            {number}
             {renderedBooks}
         </div>
     );
